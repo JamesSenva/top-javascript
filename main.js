8ba50f7d6b3d3946b45bd693198f56cc18e9d228
JamesSenva <jamessenva@gmail.com>
2022-04-26 22:32:52 +0530

    index.html and main.js files for javascript practices

---
+++ main.js
@@ -0,0 +1,22 @@
+
+/////// practice 1,2,3
+// let printSol = (4 + 6 + 9) / 77;
+// console.log(printSol);
+// console.log(printSol.toFixed(5));
+
+
+/////// practice 4
+// let a = 10;
+// console.log(a);
+// console.log(9 * a);
+
+// let b = 7 * a;
+// console.log(b);
+
+
+/////// practice 5
+const max = 57;
+let actual = max -13;
+let percentage = actual / max;
+console.log(percentage);
+console.log(percentage.toFixed(4));
