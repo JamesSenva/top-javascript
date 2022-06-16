666665288682666be2afc9f2bde4c7e14f8ba102
JamesSenva <jamessenva@gmail.com>
2022-06-16 22:40:36 +0530

    takes a string and returns the last character of the string

---
+++ foundation-fundamentals-part3/lastletter.js
@@ -0,0 +1,9 @@
+// Write a function called lastLetter that takes a string and returns the very last letter of that string:
+
+//     lastLetter("abcd") should return "d"
+
+function lastLetter(str){
+    console.log( str.slice(-1));
+}
+
+lastLetter('JAMES');
